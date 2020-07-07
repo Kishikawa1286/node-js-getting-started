5b041dd5fec603c11ebcda189b98382d945e8e6e
Kishikawa1286 <k.ishikawa.gude@gmail.com>
2020-07-07 18:24:48 +0900

    lineでオウム返しできるように

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const line = require("@line/bot-sdk");
+
+const config = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_SECRET_KEY
+};
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/g/', (req, res) => res.json({ method: "get" }))
-  .post('/p/', (req, res) => res.json({ method: "posted" }))
-  .post("/hook/", (req, res) => res.json({ test: "hook" }))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  // .use(express.static(path.join(__dirname, 'public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  // .get('/g/', (req, res) => res.json({ method: "get" }))
+  // .post('/p/', (req, res) => res.json({ method: "posted" }))
+  .post("/linehook/", line.middleware(config), (req, res) => lineBot(req, res))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const lineBot = (req, res) => {
+  res.status(200).end();
+  const events = req.body.events;
+  const promises = [];
+  for (const event of events) {
+    promises.push(generateText(event));
+  }
+  Promise.all(promises).then(console.log("pass"));
+};
+
+const generateText = async (event) => {
+  const pro =  await client.getProfile(ev.source.userId);
+  return client.replyMessage(ev.replyToken, {
+    type: "text",
+    text: `${pro.displayName}\n${ev.message.text}`
+  });
+};
