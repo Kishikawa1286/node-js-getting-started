88ab80da60893b17382a055c8abe66e3e1c7c79d
Kishikawa1286 <k.ishikawa.gude@gmail.com>
2020-07-10 01:01:02 +0900

    表示を変更

--- discordbot.js
+++ discordbot.js
@@ -10,16 +10,17 @@ const lineClient = new line.Client(lineConfig);
 const discordClient = new discord.Client();
 
 discordClient.on('message', async (event) => {
-  console.log(`channel id: ${event.channel.id}`);
   try {
     if (
       event.author.username != 'Mr.Wombat'
+      // 自前のサーバーのチャンネルのID
       && event.channel.id == '720810848003686500'
       && !event.author.bot
     ) {
       await lineClient.pushMessage(
+        // 自前のグループのID
         'C00292191febefd43a58ad477709683ea',
-        { type: 'text', text: `${event.author.username}\n${event.content}` }
+        { type: 'text', text: `(${event.author.username})\n${event.content}` }
       );
     }
   } catch(error) {
