65761e42c692e0ca543f7738e95e46c3282ef481
Kishikawa1286 <k.ishikawa.gude@gmail.com>
2020-07-10 00:41:14 +0900

    discord->line完成

--- discordbot.js
+++ discordbot.js
@@ -10,12 +10,16 @@ const lineClient = new line.Client(lineConfig);
 const discordClient = new discord.Client();
 
 discordClient.on('message', async (event) => {
-  if (event.author.username != 'Mr.Wombat') {
-    await lineClient.pushMessage(
-      'C00292191febefd43a58ad477709683ea',
-      { type: 'text', text: `${event.author.username}\n${event.content}` }
-    );
-  }
+  try {
+    if (event.author.username != 'Mr.Wombat') {
+      await lineClient.pushMessage(
+        'C00292191febefd43a58ad477709683ea',
+        { type: 'text', text: `${event.author.username}\n${event.content}` }
+      );
+    }
+  } catch(error) {
+    console.error(error);
+  } 
 });
 
 discordClient.login(process.env.DISCORD_ACCESS_TOKEN);
