ac4e6c3bb94bbabb3278928f5a50d98af7c4241b
Kishikawa1286 <kentaroishikawa1286@gmail.com>
2020-08-24 18:52:48 +0900

    修正

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const line = require('@line/bot-sdk');
 const axios = require('axios');
 const fs = require('fs');
+const Gyazo = require("gyazo-api");
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,6 +12,8 @@ const lineConfig = {
   channelSecret: process.env.LINE_SECRET_KEY
 };
 const lineClient = new line.Client(lineConfig);
+
+const gyazoClient = Ggyazo(process.env.GYAZO_ACCESS_TOKEN);
 
 const webhookUrl = process.env.DISCORD_WEBHOOK_URL
 const webhookConfig = {
@@ -43,35 +46,48 @@ const generatePostData = async (event, username) => {
         content: `${username} send a sticker.`,
       };
     case 'image':
-      // const response = await axios.get(
-      //   `https://api.line.me/v2/bot/message/${event.message.id}/content`,
-      //   {
-      //     responseType: 'arraybuffer',
-      //     headers: {
-      //       'Content-Type': 'image/jpg',
-      //       Authorization: `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
-      //     },
-      //   },
-      // );
+      const responseOfGettingImage = await axios.get(
+        `https://api.line.me/v2/bot/message/${event.message.id}/content`,
+        {
+          responseType: 'arraybuffer',
+          headers: {
+            'Content-Type': 'image/jpg',
+            Authorization: `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
+          },
+        },
+      );
 
-      // if (response.status !== 200) {
-      //   throw Error(`failed to get image  status: ${response.status}`);
-      // }
-      // console.log('successfully get image');
+      if (responseOfGettingImage.status !== 200) {
+        throw Error(`Failed to get image. status: ${responseOfGettingImage.status}`);
+      }
+      console.log('Successfully get image.');
 
-      // const encodedData = Buffer.from(response.data, 'binary');
-      // try {
-      //   fs.unlinkSync('./image.jpg'); // 古い image.jpg を消す
-      // } catch(error) {
-      //   console.log('image.jpg does not exist.');
-      // }
-      // fs.writeFileSync('./image.jpg', encodedData, 'binary');
+      const encodedData = Buffer.from(responseOfGettingImage.data, 'binary');
+      try {
+        fs.unlinkSync('./image.jpg'); // 古い image.jpg を消す
+      } catch(error) {
+        console.log('image.jpg does not exist.');
+      }
+      fs.writeFileSync('./image.jpg', encodedData, 'binary');
+
+      const responseOfUploadingImage = gyazoClient.upload(
+        './image.jpg',
+        {
+          title: 'image',
+          desc: 'uploaded from mrwombat',
+        },
+      );
+      if (responseOfUploadingImage.status !== 200) {
+        throw Error(`Failed to upload image to Gyazo. status: ${responseOfUploadingImage.status}`);
+      }
+      console.log('Successfully uploaded image to Gyazo.');
+      const gyazoUrl = responseOfUploadingImage.data.permalink_url;
 
       return {
         username,
         embeds: [{
           image: {
-            url: 'https://qiita-user-profile-images.imgix.net/https%3A%2F%2Fqiita-image-store.s3.amazonaws.com%2F0%2F189206%2Fprofile-images%2F1524018353?ixlib=rb-1.2.2&auto=compress%2Cformat&lossless=0&w=48&s=3af99c4a18c5b436ebd19a88e7d370f0',
+            url: gyazoUrl,
           }
         }],
       };
