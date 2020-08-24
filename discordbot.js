960054b9541945002ea955349a70e18c82ab82de
Kishikawa1286 <kentaroishikawa1286@gmail.com>
2020-08-24 22:18:39 +0900

    修正

--- discordbot.js
+++ discordbot.js
@@ -12,7 +12,7 @@ const discordClient = new discord.Client();
 const generateMessage = (event) => {
   const content = event.content;
   const attachments = [event.attachments.values()];
-  console.log(attachments[0]);
+  console.log(attachments[0][0]);
   // 画像あり
   if (attachments[0]) {
     const images = attachments.map((attachment) => {
